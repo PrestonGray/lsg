621ae72257205300fd6bb5bd7fa510ae593a8ae0
Preston Gray <preston.gray@plowtech.net>
2022-06-01 11:45:50 -0500

    Start default config write routine

--- src/LSG/Config.hs
+++ src/LSG/Config.hs
@@ -11,6 +11,7 @@ import Dhall ((>$<), (>*<))
 import qualified Dhall
 import GHC.Generics (Generic)
 import qualified System.Console.ANSI as ANSI
+import qualified System.Directory as System
 
 -- | The options configurable from ~/.lsgrc
 data LSGConfig = LSGConfig
@@ -25,29 +26,20 @@ instance Dhall.FromDhall LSGConfig where
         <$> Dhall.field "options" Dhall.auto
         <*> Dhall.field "color" (fmap toANSIColor (Dhall.auto :: Dhall.Decoder Color))
 
-instance Dhall.ToDhall LSGConfig where
-  injectWith _ =
-    Dhall.recordEncoder $
-      adapt
-        >$< Dhall.encodeField "options"
-        >*< Dhall.encodeField "color"
-    where
-      adapt LSGConfig {..} = (lsgOptions, fromANSIColor lsgMatchColor)
-
 -- | Options outlines possible command line arguments
 -- All options default to False and are configurable in ~/.lsgrc, excluding version
 data Options = Options
   { optAll :: Bool
   , optFiles :: Bool
-  , optColor :: Bool
+  , optDirectories :: Bool
   , optHidden :: Bool
-  -- Version is not configurable
-  , optVersion :: Bool
+  , optExecutables :: Bool
+  , optNonExecutables :: Bool
   , optCaseMatch :: Bool
   , optStartsWith :: Bool
-  , optDirectories :: Bool
-  , optExecutables :: Bool
-  , optFilterExecutables :: Bool
+  , optColor :: Bool
+  -- Version is not configurable
+  , optVersion :: Bool
   } deriving (Eq, Show)
 
 instance Dhall.FromDhall Options where
@@ -56,29 +48,14 @@ instance Dhall.FromDhall Options where
       Options
         <$> Dhall.field "searchAll" Dhall.auto
         <*> Dhall.field "matchFilesOnly" Dhall.auto
-        <*> Dhall.field "useColor" Dhall.auto
-        <*> Dhall.field "matchHiddenOnly" Dhall.auto
-        <*> pure False
-        <*> Dhall.field "caseSensitive" Dhall.auto
-        <*> Dhall.field "matchBeginning" Dhall.auto
         <*> Dhall.field "matchDirectoriesOnly" Dhall.auto
+        <*> Dhall.field "matchHiddenOnly" Dhall.auto
         <*> Dhall.field "matchExecutablesOnly" Dhall.auto
         <*> Dhall.field "matchNonExecutablesOnly" Dhall.auto
-
-instance Dhall.ToDhall Options where
-  injectWith _ =
-    Dhall.recordEncoder $
-      adapt
-        >$< Dhall.encodeField "searchAll"
-        >*< Dhall.encodeField "matchFilesOnly"
-        >*< Dhall.encodeField "useColor"
-        >*< Dhall.encodeField "matchHiddenOnly"
-        >*< Dhall.encodeField "caseSensitive"
-        >*< Dhall.encodeField "matchBeginning"
-        >*< Dhall.encodeField "matchDirectoriesOnly"
-        >*< Dhall.encodeField "matchExecutablesOnly"
-        >*< Dhall.encodeField "matchNonExecutablesOnly"
-    where adapt Options {..} = (optAll, (optFiles, (optColor, (optHidden, (optCaseMatch, (optStartsWith, (optDirectories, (optExecutables, optFilterExecutables))))))))
+        <*> Dhall.field "caseSensitive" Dhall.auto
+        <*> Dhall.field "matchBeginning" Dhall.auto
+        <*> Dhall.field "useColor" Dhall.auto
+        <*> pure False
 
 -- | An isomorphism for ANSI.Color to prevent an orphan FromDhall instance
 data Color
@@ -93,8 +70,6 @@ data Color
   deriving (Eq, Show, Generic)
 
 instance Dhall.FromDhall Color
-
-instance Dhall.ToDhall Color
 
 fromANSIColor :: ANSI.Color -> Color
 fromANSIColor ANSI.Black = Black
@@ -115,3 +90,50 @@ toANSIColor Blue = ANSI.Blue
 toANSIColor Magenta = ANSI.Magenta
 toANSIColor Cyan = ANSI.Cyan
 toANSIColor White = ANSI.White
+
+-- | Creates the default ~/.lsgrc config file
+-- Prompts the user to overwrite if the file already exists
+createDefaultConfig :: IO ()
+createDefaultConfig = do
+  home <- System.getHomeDirectory
+  let filename = ".lsgrc"
+  fileExists <- System.doesFileExist (home <> "/" <> filename)
+  if fileExists
+     then putStrLn "File exists"
+     else
+      writeFile
+        filename
+        defaultConfigContents
+
+-- | The default configuration file contents to write
+defaultConfigContents :: String
+defaultConfigContents =
+  "-- Possible color options\n"
+  <> "let Color : Type = < Black | Red | Green | Yellow | Blue | Magenta | Cyan | White >\n"
+  <> "\n"
+  <> "in  { options =\n"
+  <> "       {\n"
+  <> "         -- Match all file types, including hidden files\n"
+  <> "         searchAll = False\n"
+  <> "         -- Match files only\n"
+  <> "       , matchFilesOnly = False\n"
+  <> "         -- Match directories only\n"
+  <> "       , matchDirectoriesOnly = False\n"
+  <> "         -- Match hidden files (.*) only\n"
+  <> "       , matchHiddenOnly = False\n"
+  <> "         -- Match executable files only\n"
+  <> "         -- NOTE: Mutually exclusive with matchNonExecutablesOnly\n"
+  <> "       , matchExecutablesOnly = False\n"
+  <> "         -- Match non-executable files only\n"
+  <> "         -- NOTE: Mutually exclusive with matchExecutablesOnly\n"
+  <> "       , matchNonExecutablesOnly = False\n"
+  <> "         -- Enable case sensitive search\n"
+  <> "       , caseSensitive = False\n"
+  <> "         -- Match files that begin with the pattern\n"
+  <> "       , matchBeginning = False\n"
+  <> "         -- Color the matched pattern in the output\n"
+  <> "       , useColor = False\n"
+  <> "       }\n"
+  <> "     -- Specify the output color\n"
+  <> "   , color = Color.Green\n"
+  <> "   }"
